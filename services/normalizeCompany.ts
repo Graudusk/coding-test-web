c3fffbddd8f0f3d3e5abdaf7233a4805427eae7f
Eric johansson <johanssoneric5@gmail.com>
2025-12-03 21:18:27 +0100

    Add backend data processing logic

---
+++ services/normalizeCompany.ts
@@ -0,0 +1,18 @@
+import { Company } from '@/types/Company';
+import { RawCompany } from '@/types/RawCompany';
+
+export function normalizeCompany(company: RawCompany): Company {
+  const image = company.iconUrl ?? company.logoDarkUrl ?? company.logoLightUrl ?? null;
+
+  return {
+    id: company.companyId,
+    image,
+    name: company.companyName,
+    country: company.companyCountry,
+    ticker: company.companyTicker,
+    displayName: company.displayName,
+    description: company.description,
+    infoUrl: company.infoUrl ?? null,
+    brandColor: company.colorSettings?.brandColor ?? null,
+  };
+}
