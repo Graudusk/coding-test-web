d48ab4dcd4ca6ee8a7bd259ebf1213f9765fcb29
Eric johansson <johanssoneric5@gmail.com>
2025-12-03 21:14:23 +0100

    Move companies.ts into pages/api/v1 folder and add query parameters handling, error handling and asynchronous fetching

---
+++ pages/api/v1/companies.ts
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { fetchTrendingCompanies } from '@/services/companyService';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  // res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const { limit = 10, offset = 0 } = req.query;
+
+    const startIndex = Number(offset);
+    const endIndex = startIndex + Number(limit);
+    const companies = await fetchTrendingCompanies();
+    const paginatedCompanies = companies.slice(startIndex, endIndex);
+
+    res.status(200).json(paginatedCompanies);
+  } catch (error: any) {
+    console.error('API Error:', error);
+    res.status(500).json({ error: error.message || 'Internal Server Error' });
+  }
+}
