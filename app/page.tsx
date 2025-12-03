7bf3032eb63da6088d4843228ef8a358fe2eb9d7
Eric johansson <johanssoneric5@gmail.com>
2025-12-03 21:33:19 +0100

    Update app,tsx to use CompanyListWrapper, LoadingSkeleton and Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,20 @@
-"use client";
+import { CompanyListWrapper } from '@/components/CompanyListWrapper/CompanyListWrapper';
+import { LoadingSkeleton } from '@/components/LoadingSkeleton/LoadingSkeleton';
+import { Inter } from '@next/font/google';
+import { Suspense } from 'react';
 
-import { Inter } from "@next/font/google";
-import { useEffect, useState } from "react";
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-export default function Home() {
-  const [stuff1, setStuff1] = useState<any>([]);
-  useEffect(() => {
-    // declare the data fetching function
-    const fetchData = async () => {
-      const data = await fetch("/api/companies");
-      const data2 = await data.json();
-      console.log(data2);
-      setStuff1(data2);
-    };
-
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-  }, []);
-
+export default function Page() {
   return (
     <main>
       <h2 className={inter.className}>Quartr</h2>
-      <p className={inter.className}>Trending companies</p>
-      <p>{JSON.stringify(stuff1)}</p>
+      <p className={`${inter.className} lead`}>Trending companies</p>
+
+      <Suspense fallback={<LoadingSkeleton />}>
+        {/* @ts-expect-error Async Server Component */}
+        <CompanyListWrapper />
+      </Suspense>
     </main>
   );
 }
