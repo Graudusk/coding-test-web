483a5f2bc0c246d54ae8118d215b2de0f94a4b72
Eric johansson <johanssoneric5@gmail.com>
2025-12-05 11:12:38 +0100

    refactor: update company card skeleton styles and structure

--- components/CompanyCardSkeleton/CompanyCardSkeleton.tsx
+++ components/CompanyCardSkeleton/CompanyCardSkeleton.tsx
@@ -1,21 +1,20 @@
-// app/components/CompanyCard/CompanyCardSkeleton.tsx
-import styles from '@components/CompanyCard/CompanyCard.module.scss';
+import cardStyles from '@components/CompanyCard/CompanyCard.module.scss';
 import { Inter } from '@next/font/google';
 import skeletonStyles from './CompanyCardSkeleton.module.scss';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function CompanyCardSkeleton() {
   return (
-    <div className={`${styles.companyCard} ${skeletonStyles.skeleton}`} aria-hidden="true">
-      <div className={`${styles.logo} ${skeletonStyles.logoSkeleton}`} />
+    <div className={`${cardStyles.companyCard} ${skeletonStyles.skeleton}`} aria-hidden="true">
+      <div className={cardStyles.companyCardContent}>
+        <div className={skeletonStyles.logoSkeleton} />
 
-      <div className={`${styles.info} ${skeletonStyles.infoSkeleton}`}>
-        <div className={`${inter.className} ${skeletonStyles.titleSkeleton}`} />
-        <div className={`${inter.className} ${skeletonStyles.descriptionSkeleton}`} />
-      </div>
+        <div className={skeletonStyles.infoSkeleton}>
+          <div className={`${inter.className} ${skeletonStyles.titleSkeleton}`} />
+          <div className={`${inter.className} ${skeletonStyles.descriptionSkeleton}`} />
+        </div>
 
-      <div className={styles.icon}>
-        <div className={skeletonStyles.iconSkeleton} />
+        <div className={`${cardStyles.toggleButton} ${skeletonStyles.buttonSkeleton}`} />
       </div>
     </div>
   );
